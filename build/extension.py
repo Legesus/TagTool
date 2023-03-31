da04e63ecae762d0042f395facecd8650ad4c7ed
Adam <adamnasir24@gmail.com>
2023-04-01 06:24:57 +0800

    Tried implementing snippet

--- build/extension.py
+++ build/extension.py
@@ -3,16 +3,15 @@
 import sys
 import vscode
 
-ext = vscode.Extension(name="TestTool", display_name="Test Tool", version="0.1.0")
+ext = vscode.Extension(name = "TestTool", display_name = "Test Tool", version = "0.0.1")
 
 @ext.event
 def on_activate():
     return f"The Extension '{ext.name}' has started"
 
-
 @ext.command()
 def hello_world():
-    return vscode.window.show_info_message(f"Hello World from {ext.name}")
+    vscode.window.show_info_message(f'Hello World from {ext.name}')
 
 
 
